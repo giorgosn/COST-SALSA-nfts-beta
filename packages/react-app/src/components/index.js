2e6f27444330d10d54df097b4b90ff50fd72dae3
giorgosn <sdi1400127@di.uoa.gr>
2021-10-16 22:52:42 +0300

    Initialize feature/449/use-eth-components

---
+++ packages/react-app/src/components/index.js
@@ -0,0 +1,10 @@
+export { default as BytesStringInput } from "./BytesStringInput";
+export { default as Header } from "./Header";
+export { default as Provider } from "./Provider";
+export { default as Ramp } from "./Ramp";
+export { default as Swap } from "./Swap";
+export { default as ThemeSwitch } from "./ThemeSwitch";
+export { default as Timeline } from "./Timeline";
+export { default as TokenBalance } from "./TokenBalance";
+export { default as L2Bridge } from "./L2Bridge";
+export { default as Contract } from "./Contract";
